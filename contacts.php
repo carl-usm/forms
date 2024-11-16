1d0f25e5dc6d1a9ff6ff1fd2e3e943bbf38695ed
carl-usm <s.cccastro@usm.edu.ph>
2024-11-16 19:48:26 +0800

    Add files via upload

---
+++ contacts.php
@@ -0,0 +1,12 @@
+<?php
+
+if(isset($_POST['submit'])){
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['firstname'];
+    $message = $_POST['subject'];
+
+    $text = "Mail from".$firstname." ".$lastname.".\n\n".$message;
+
+    mail("carlo.castro.320401@gmail.com", "contact-website", $text);
+    header("Location: contacts.php?mailsent");
+}
