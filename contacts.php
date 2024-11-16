1c58b9edc0d8f951fe2a2185ac6df5ff0f0dfa25
carl-usm <s.cccastro@usm.edu.ph>
2024-11-16 20:04:15 +0800

    Update contacts.php

--- contacts.php
+++ contacts.php
@@ -9,6 +9,20 @@ if(isset($_POST['submit'])){
 
     mail("carlo.castro.320401@gmail.com", "contact-website", $text);
     header("Location: contacts.php?mailsent");
+
+    echo'
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>done</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta charset="UTF-8">
+        </head>
+        <body>
+            <h1>Thanks for messaging</h1>
+        </body>
+    </html>
+    '
 }
 
 $>
